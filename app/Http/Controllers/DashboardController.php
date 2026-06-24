92f22ef2f693fc0010d0e7a8cb04084eea354eb7
sookoo19 <kouhei20001011@gmail.com>
2026-06-24 11:09:46 +0100

    style: import User in DashboardController (pint fully_qualified_strict_types)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -15,7 +16,7 @@ class DashboardController extends Controller
 
     public function partner(): Response
     {
-        /** @var \App\Models\User $user */
+        /** @var User $user */
         $user = Auth::user();
 
         return Inertia::render('Dashboard/Partner', [
